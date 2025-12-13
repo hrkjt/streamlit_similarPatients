a91590b70ce3c2b8528b0246d7eb0926cbc27c86
hrkjt <168939068+hrkjt@users.noreply.github.com>
2025-12-13 16:59:27 +0900

    Update app.py
    dfpt_w.loc[dfpt_w.index[0], "月齢"] *= 100  #月齢重みを100倍

--- app.py
+++ app.py
@@ -385,7 +385,7 @@ def tx_rate_st(dfpt, df_first=df_first, treated_set=None, n=30):
     if dfpt_w["月齢"].iloc[0] < dfpt_w.T.max().iloc[0]:
         dfpt_w.loc[dfpt_w.index[0], "月齢"] = dfpt_w.T.max().iloc[0]
 
-    dfpt_w.loc[dfpt_w.index[0], "月齢"] *= 10  #月齢重みを10倍
+    dfpt_w.loc[dfpt_w.index[0], "月齢"] *= 100  #月齢重みを100倍
 
     df_first2 = age_restriction(df_first, dfpt)
     df_first_temp = parameter_restriction(df_first2, dfpt)  # ※この関数は別途定義が必要
